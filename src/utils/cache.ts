f8a46d06d16c9102f300cb32807b1ca201ebc514
Nat <natalyaiskanderova@gmail.com>
2024-06-19 16:38:04 +0200

    change localStorage to Session

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -2,18 +2,18 @@
 const cache = {
     get: (key: string): Set<string> | undefined => {
       if (typeof window !== 'undefined') {
-        const value = localStorage.getItem(key);
+        const value = sessionStorage.getItem(key);
         return value ? new Set(JSON.parse(value)) : undefined;
       }
     },
     set: (key: string, value: Set<string>) => {
       if (typeof window !== 'undefined') {
-        localStorage.setItem(key, JSON.stringify(Array.from(value)));
+        sessionStorage.setItem(key, JSON.stringify(Array.from(value)));
       }
     },
     remove: (key: string) => {
       if (typeof window !== 'undefined') {
-        localStorage.removeItem(key);
+        sessionStorage.removeItem(key);
       }
     },
   };
