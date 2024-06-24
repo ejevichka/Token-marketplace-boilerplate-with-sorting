d7d06084d9f51ff2409bf19a335d560833a2bed4
Nat <natalyaiskanderova@gmail.com>
2024-06-24 17:11:39 +0200

    update filters +  add data inreachment from /chains endpoint

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,10 @@
 import * as z from "zod";
 import { EVMAddress } from "~/domains/evm";
+import {ExtendedChain} from "@lifi/types";
 
 const API_URL = "https://li.quest/v1/tokens";
+const CHAIN_API_URL = 'https://li.quest/v1/chains';
+
 
 // @todo find this model in LIFI codebase
 export const TokenSchema = z.object({
@@ -13,6 +16,7 @@ export const TokenSchema = z.object({
   name: z.string(),
   priceUSD: z.string(),
   symbol: z.string(),
+  chainType: z.string().optional()
 });
 
 export type TToken = z.infer<typeof TokenSchema>;
@@ -24,13 +28,38 @@ export const TokenFilterSchema = z.object({
 
 export type TTokenFilter = z.infer<typeof TokenFilterSchema>;
 
-export const fetchTokens = async () => {
-  const res = await fetch(API_URL);
+const fetchChainData = async (): Promise<{ [key: number]: string }> => {
+  const res = await fetch(`${CHAIN_API_URL}?chainTypes=EVM%2CSVM`);
+  if (!res.ok) {
+    throw new Error(`Failed to fetch chain data: ${res.statusText}`);
+  }
+  const data = await res.json();
+  const chainData: ExtendedChain[] = data.chains;
+
+  const chainTypeMapping: { [key: number]: string } = {};
+  chainData.forEach((chain) => {
+    chainTypeMapping[chain.id] = chain.chainType;
+  });
+
+  return chainTypeMapping;
+};
+
+export const fetchTokens = async (): Promise<TToken[]> => {
+  const chainTypeMapping = await fetchChainData();
+
+  const res = await fetch(`${API_URL}?chainTypes=EVM%2CSVM`);  // By default API returns only EVM tokens
   if (!res.ok) {
     throw new Error(`Failed to fetch tokens: ${res.statusText}`);
   }
   const data = await res.json();
   const tokenData = data.tokens;
-  const tokensArray = Object.values(tokenData).flat().map((x) => TokenSchema.parse(x)) as TToken[];
+
+  const tokensArray: TToken[] = Object.entries(tokenData).flatMap(([chainId, tokens]: [string, any]) => {
+    return tokens.map((token: any) => ({
+      ...token,
+      chainType: chainTypeMapping[parseInt(chainId, 10)] || 'Unknown',
+    }));
+  });
+
   return tokensArray;
 };
